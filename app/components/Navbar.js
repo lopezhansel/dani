5543af5eefca9817ada45cf4175c4d298f1a52c6
Hansel Lopez <lopezhansel@gmail.com>
2016-04-25 01:36:07 -0600

    Using jQuery, Semantic-UI, Custom features into Navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,30 +1,50 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {Link} from 'react-router';
 import Logo from './Logo';
-class Navbar extends React.Component {
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  componentDidMount() {
+    setTimeout(letjQueryLoad => {
+      this.$inky = $('.inky');
+      this.$rightMenu = $('#nav');
+      this.$oldActiveItem = this.$rightMenu.find('.active');
+      this.$inky.css({ left: this.$oldActiveItem.offset().left, width: this.$oldActiveItem.width(), display:'none' });
+    }, 200);
+  }
+  
+  onClick(ev) {
+    let newActiveItem = ev.currentTarget;
+    let {$inky,$rightMenu,$oldActiveItem} = this;
+    
+    $oldActiveItem.removeClass('active');
+    $inky.css({ display: 'block' }).after(a=> $inky.css({ left: newActiveItem.offsetLeft, width: newActiveItem.clientWidth }));
+    setTimeout(letTransitionFinish => {
+      this.$oldActiveItem = $rightMenu.find(newActiveItem).addClass('active');
+      $inky.css({ display: 'none' });
+    }, 300);
+  }
   render() {
     return (
-      <div className=" top-bar">
-        <div className="wrapper">
-          <div className="top-bar-left">
-            <ul className="menu">
-              <li ></li>
-              <Link to={'/'}>
-                <Logo ></Logo>
-              </Link>
-            </ul>
-          </div>
-          <div className="top-bar-right">
-            <ul className="menu">
-              <li >
-              <Link to={'/about'}>About</Link>
-              </li>
-              <li ><a href="#work">Work</a></li>
-              <li ><a href="#contact">Contact</a></li>
-            </ul>
+      <nav id="navbar" className="ui ui  large secondary network menu inverted fixed">
+        <div className="ui container">
+          <div id="nav" className="ui large secondary network menu inverted fluid pointing">
+            {<Link onClick={this.onClick} className="ui item" to={'/'}>
+              <Logo  ></Logo>
+            </Link>}
+            <div className="right menu">
+              <div className="inky"></div>
+              <Link onClick={this.onClick} className="ui item" to={'/'}>Home</Link>
+              <Link onClick={this.onClick} className="ui item active" to={'/about'}>About</Link>
+              <a onClick={this.onClick} className="ui item">Work</a>
+              <a onClick={this.onClick} className="ui item">Contact</a>
+            </div>
           </div>
         </div>
-      </div>
+      </nav>
     );
   }
 }
